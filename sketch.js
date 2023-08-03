53cf667f5abe713bf6c57bc022410d64268d97d9
STbryan <138117622+STbryan@users.noreply.github.com>
2023-08-03 08:07:15 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    2 createCanvas(600, 600); 
+    background("black")
+    {
+
+    Function draw() ( 
+    stroke("blue");
+    fill("red");
+    
+    if(mouseisPressed){ 
+        rect(mouseX, mouseY, 20, 35);
+        {
+            
+            {
