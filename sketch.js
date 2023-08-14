b11e21385fee1d8b227ccf873223f39a2e4657cf
STbryan <138117622+STbryan@users.noreply.github.com>
2023-08-14 11:54:55 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-    2 createCanvas(600, 600); 
-    background("black"); 
-    {
+  2 createCanvas(600, 600); 
+  background("black"); 
+}
 
-    Function draw()  {
-    stroke("blue");
-    fill("red");
+Function draw()  {
+  stroke("blue");
+  fill("red");
+}
+  if(mouseisPressed){ 
+    rect(mouseX, mouseY, 20, 35);
+  }
     
-    if(mouseisPressed){ 
-        rect(mouseX, mouseY, 20, 35);
-        {
-            
-            {
+}    
